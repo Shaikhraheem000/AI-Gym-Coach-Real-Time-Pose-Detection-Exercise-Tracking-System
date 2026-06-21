c2fa0363f6ded97361571285ad90f4b144b0e3eb
Shaikhraheem000 <shaikhraheemr03@gmail.com>
2026-06-21 20:23:53 +0530

    Final commit

--- App/core/base_exercise.py
+++ App/core/base_exercise.py
@@ -8,18 +8,18 @@ class BaseExercise(ABC):
         self.stage = None
 
     def calculate_angle(self, a, b, c):
-        ax, ay = a[0] - b[0], a[1] - b[1]
-        cx, cy = c[0] - b[0], c[1] - b[1]
+        ax, ay = a[0] - b[0], a[1] - b[1] 
+        cx, cy = c[0] - b[0], c[1] - b[1] 
 
-        dot = ax * cx + ay * cy
+        dot = ax * cx + ay * cy  # BA·BC
 
-        mag_a = math.sqrt(ax ** 2 + ay ** 2)
-        mag_c = math.sqrt(cx ** 2 + cy ** 2)
+        mag_a = math.sqrt(ax ** 2 + ay ** 2) # |BA|
+        mag_c = math.sqrt(cx ** 2 + cy ** 2)  # |BC|
 
         if mag_a * mag_c == 0:
             return 0.0
 
-        cos_angle = max(-1.0, min(1.0, dot / (mag_a * mag_c)))
+        cos_angle = max(-1.0, min(1.0, dot / (mag_a * mag_c))) # cos(θ) = (BA·BC) / (|BA||BC|).
 
         return math.degrees(math.acos(cos_angle))
 
